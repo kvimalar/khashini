fb08b78a3cc0b3f32710295d2dccfa5111cca512
kvimalar <khashini.viamalarajah@ryerson.ca>
2015-10-23 17:22:16 -0400

    Added a new like to the program

---
+++ khashini/src/khashini.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : khashini.cpp
+// Author      : khahsini
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello" << endl;
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
